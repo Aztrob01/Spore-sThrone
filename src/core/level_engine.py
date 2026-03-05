0b5d229b630a5567f6f1f9968a980b35252e062e
Aztromyy <galxy.sup@gmail.com>
2026-03-05 19:27:10 -0300

    Update core mechanics

--- src/core/level_engine.py
+++ src/core/level_engine.py
@@ -1,37 +1,59 @@
 import pygame, random
 
 from root.settings       import *
-from entities.classes    import * #! removable
-from entities.enemy      import * #! removable
-from entities.characters import Characters
+from root.utils import image_load
+from core.level import *
+from core.player_core  import PlayerCore
 from entities.entities   import Entities
-from core.combat_engine  import CombatSys
-
-# Control any combat screen
-
-
-class World:
-    def __init__(self):
-        pass
+from core.combat_engine  import CombatEngine
+from entities.enemy import *
 
 
-class LevelCreator:
-    def __init__(self):
+class LvlDataHandler:
+    def __init__(self, data):
+        self.levelnum = data['lvl']
+        self.levelname = data['lvl_name']
+        self.battleground = pygame.image.load(data['battleground'])
+        self.battleground = pygame.transform.scale(self.battleground, (WIDTH, HEIGHT))
+        
+        self.entities  = data['entities_list']
+        self.arealevel = data['area_level']
+
+    def entities_randomize(self, n):
+        import random
+        from entities.entities import Entities
+        
+        w = self.entities
+        x = []
+        
+        for r in range(0, n):
+            x.append(Entities(random.choice(w)()))
+            x[-1].mlvl = random.randint(self.arealevel[0], self.arealevel[1])
+        return x
+
+class LevelEngine:
+    def __init__(self, eventtoken):
         self.display = pygame.display.get_surface()
+        self.player  = PlayerCore()
+        self.token   = eventtoken
         
-        self.WORLD   = World()
-        self.COMBAT  = CombatSys('./assets/image/battlegrounds/sideviewerII.png')
+        self.data = {
+            'lvl': 2,
+            'lvl_name': 'Training Area',
+            'area_level': [1, 5],
+            'battleground': './assets/image/battlegrounds/sideviewerII.png',
+            'entities_list': [gob, dmy] 
+        }
+        self.level   = LvlDataHandler(self.data)
+
+        self.explore = None #! Correct Later
+        self.combat  = CombatEngine(self.level, self.player, self.token)
+        self.state   = 'fight'
+
         
-        self.WORLD_STAGE = None
 
     def play(self):
-        if self.COMBAT.PLAYER_TEAM == []:
-            for x in range(0, 3):
-                target = random.choice(LIST)
-                self.COMBAT.PLAYER_TEAM.append(Characters(LIST[x]()))
-                self.COMBAT.PLAYER_TEAM[x].index = x
-        if self.COMBAT.ENEMY_TEAM == []:
-            for x in range(0, 3):
-                self.COMBAT.ENEMY_TEAM.append(Entities(Goblin()))
-                self.COMBAT.ENEMY_TEAM[x].index = x
-        self.COMBAT.start()
+        if self.state == 'fight':
+            self.combat.start()
+        elif self.state == 'explore':
+            pass
