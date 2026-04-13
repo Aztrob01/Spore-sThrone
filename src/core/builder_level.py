b9967cac5d6efc4a1a63e689c8f9da5b39ade777
Aztromyy <galxy.sup@gmail.com>
2026-04-13 16:24:41 -0300

    Reestructured generate_enemies()
    generate_enemies was causing too much difficulty to resolve, due my mistakes while creating that. Now the generate_enemies dont generate white spaces (None), 0 outputs or 6+ entities error while refactored with 3 more steps while rendering creatures and a creature check to make this precisely.

--- src/core/builder_level.py
+++ src/core/builder_level.py
@@ -10,57 +10,44 @@ class LevelBuilder:
         background = self.data_level['battleground']
         return background
     
-    def generate_enemies(self):
-        print(self.data_level)
-        slots  = random.randint(2, 5)
-        entry = []
-
-        for ents in self.data_level['enemies']:
-            entry.append(self.data_level['enemies'][ents])
-
-        for numbers, monsters in enumerate(entry):
-            for nums, ents in enumerate(ENEMIES):
-                if entry[numbers][0] not in ENEMIES:
-                    print('Removing', entry[numbers][0], 'from list')
-                    entry.remove(entry[numbers])
-
-        entry.sort(key=lambda target: target[2])
-
+    def generate_enemies(self, value=0):
+        if value > 5:
+            raise ValueError('The value input is higher than 5.')
+        
+        slots = value if value > 0 else random.randint(2, 5)
         output = []
 
-        for i in range(slots):
-            j = i - 1
-            print('--------------------------------')
-            error = 0
-            ch = random.uniform(0, 1)
-            ch = round(ch, 2)
-            print(f'Dinamic entrance started with {j}, {ch}% in a total of {slots} tries' )
+        entities = []
+        for nums in self.data_level['enemies']:
+            entities.append(self.data_level['enemies'][nums])
 
-            for objects in entry:
-                print('-----------------------------\nChecking for objects in entry. Value', objects)
-                if ch >= entry[-1][2]:
-                    output.append(None)
-                elif ch <= objects[2]:
-                    obj = Entities(ENEMIES[objects[0]]())
-                    print(f'{ch}% defined for {obj.job.data['info']['codename']}')
-                    obj.profile.stats['level']['current'] = random.randint(objects[1][0], objects[1][1])
-                    print('Entity Object created.')
-                    output.append(obj)
-                    print(output)
-                    print(f'Appended {obj.job.data['info']['codename']} at position {j} with level {obj.profile.stats['level']['current']}')
-                    print(f'{obj.job.data['info']['codename']} absorved {ch}% to appear from {objects[2]}%')
-                    break
+        entities.sort(key=lambda target : target[2])
+
+        enemies = []
+        for nums in ENEMIES:
+            enemies.append(ENEMIES[nums])
+
+        for slot in range(slots):
+            chance = round(random.uniform(0.01, 1), 2)
             
-            for objects in output:
-                if objects == None:
-                    error +=1
-                    
-            if error == len(output):
-                print('An error occurred while rendering unities. Creating a new Unity')
-                obj = Entities(ENEMIES[random.choice(entry)[0]]())
-                output.insert(0, obj)
-                if len(output) > 5:
-                    print('Higher size in Output than the supported. Removing...')
-                    output.remove(None)
-        
+            for ents in entities:
+                if chance > entities[-1][2]:
+                    cache = Entities(random.choice(enemies)())
+                    cache.profile.stats['level']['current'] = random.randint(5, 10)
+                    output.append(cache)
+                    break
+                
+                if ents[0] not in ENEMIES:
+                    continue
+
+                if chance <=ents[2]:
+                    cache = Entities(ENEMIES[ents[0]]())
+                    level = random.randint(ents[1][0], ents[1][1])
+                    cache.profile.stats['level']['current'] = level
+                    output.append(cache)
+                    break
+                
         return output
+
+        
+            
