8c5df43cfe83ad0dcee5021d692bf1fbbaa04f15
Aztromyy <galxy.sup@gmail.com>
2026-04-25 12:56:29 -0300

    Update combat_profile.py
    More info are added into self.history to match new incoming techniques change and updates on classes and entities before.

--- src/core/combat/combat_profile.py
+++ src/core/combat/combat_profile.py
@@ -15,11 +15,17 @@ class CombatProfileData:
             'resistance': { 'highest': 0, 'last_received': 0, 'total': 0, 'last_agressor': 0},
             'state': { 'last_debuff': 0, 'last_buff': 0, 'tormentor': 0, 'guardian': 0 },
             'actions': {
-                'passive': { 'times_activated': 0 },
-                'sk_0': { 'name': None, 'times_used': 0, 'total_consuption': 0 },
-                'sk_1': { 'name': None, 'times_used': 0, 'total_consuption': 0 },
-                'sk_2': { 'name': None, 'times_used': 0, 'total_consuption': 0 },
-                'sk_3': { 'name': None, 'times_used': 0, 'total_consuption': 0 },
+                'passive': { 
+                    'main': { 'name': None, 'times_activated': 0 },
+                    'family': { 'name': None, 'times_activated': 0 },
+                },
+                'active': {
+                    'sk_0': { 'name': None, 'times_used': 0, 'total_consuption': 0 },
+                    'sk_1': { 'name': None, 'times_used': 0, 'total_consuption': 0 },
+                    'sk_2': { 'name': None, 'times_used': 0, 'total_consuption': 0 },
+                    'sk_3': { 'name': None, 'times_used': 0, 'total_consuption': 0 },
+                    'sk_ult': { 'name': None, 'times_used': 0, 'total_consuption': 0 },
+                }
             },
         }
     
