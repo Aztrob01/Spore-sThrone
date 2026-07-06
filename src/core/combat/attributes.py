479a8fcee399e3ceda249d04b4f8dcca33b81d41
Aztromyy <galxy.sup@gmail.com>
2026-07-05 22:54:02 -0300

    Little update for future changes

--- src/core/combat/attributes.py
+++ src/core/combat/attributes.py
@@ -3,7 +3,7 @@
 class Attributes:
     def __init__(self, user):
         self.base     = user.job.data['stats']
-        self.received = { 'hp': 0, 'str': 0, 'dex': 0, 'knw': 0, 'lky': 0, 'res': 0 }
+        self.received = { 'vit': 0, 'str': 0, 'dex': 0, 'knw': 0, 'lky': 0, 'res': 0 }
         self.main     = { }
         for keys in self.received:
             self.main[keys] = self.base[keys] + self.received[keys]
